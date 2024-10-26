2833e241c44dcabaf5967a4b0182ee4badc26894
juanalenca <juanbarros.dev@gmail.com>
2024-10-25 22:50:16 -0300

    Iniciando o apredizado de Spring Boot

---
+++ exercicio_ssb/src/main/java/com/example/exerciciossb/ExercicioSsbApplication.java
@@ -0,0 +1,13 @@
+package com.example.exerciciossb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioSsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioSsbApplication.class, args);
+	}
+
+}
