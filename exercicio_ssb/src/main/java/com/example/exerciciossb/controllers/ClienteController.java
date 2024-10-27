34d4375c9aa497fdb5bd4e6f56497deb6bcc7196
juanalenca <juanbarros.dev@gmail.com>
2024-10-27 15:39:51 -0300

    Adicionar método para obter cliente por ID com @RequestParam

--- exercicio_ssb/src/main/java/com/example/exerciciossb/controllers/ClienteController.java
+++ exercicio_ssb/src/main/java/com/example/exerciciossb/controllers/ClienteController.java
@@ -3,6 +3,7 @@ package com.example.exerciciossb.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.exerciciossb.models.Cliente;
@@ -19,6 +20,12 @@ public class ClienteController {
 	@GetMapping("/{id}")
 	public Cliente obterClientePorId1(@PathVariable int id) { //@PathVariable: coloca o valor na url
 		return new Cliente(id, "Marta", "123.456.789-10");
+	}
+	
+	@GetMapping
+	public Cliente obterClientePorId2(
+			@RequestParam(name = "id", defaultValue = "1") int id) {
+		return new Cliente(id, "Amaro", "167.556.989-69");
 	}
 
 }
