33ec8f96848c9683d62220441f372b45dc8b9eaf
juanalenca <juanbarros.dev@gmail.com>
2024-10-27 15:28:27 -0300

    Passando um valor id pela url

--- exercicio_ssb/src/main/java/com/example/exerciciossb/controllers/ClienteController.java
+++ exercicio_ssb/src/main/java/com/example/exerciciossb/controllers/ClienteController.java
@@ -1,6 +1,7 @@
 package com.example.exerciciossb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,11 @@ public class ClienteController {
 	@GetMapping(path = "/qualquer")
 	public Cliente obterClient() {
 		return new Cliente(69, "Juan", "000.000.000-00");
+	}
+	
+	@GetMapping("/{id}")
+	public Cliente obterClientePorId1(@PathVariable int id) { //@PathVariable: coloca o valor na url
+		return new Cliente(id, "Marta", "123.456.789-10");
 	}
 
 }
