a8db0c237d4ca90e398ea728afa195e60f1249bd
juanalenca <juanbarros.dev@gmail.com>
2024-10-28 17:18:34 -0300

    Add endpoint POST para criação de novo Produto com persistência no db

---
+++ exercicio_ssb/src/main/java/com/example/exerciciossb/controllers/ProdutoController.java
@@ -0,0 +1,29 @@
+package com.example.exerciciossb.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.exerciciossb.model.entities.Produto;
+import com.example.exerciciossb.model.repositories.ProdutoRepository;
+
+@RestController
+@RequestMapping("/api/produtos")
+public class ProdutoController {
+	
+	//atribui automaticamente um object dentro do atributo produtoRepository
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
+	@PostMapping
+	public @ResponseBody Produto novoProduto(@RequestParam String nome) {
+		Produto produto = new Produto(nome);
+		//perciste o produto no mysql e o id retorna de forma correta
+		produtoRepository.save(produto);
+		return produto;
+	}
+	
+}
