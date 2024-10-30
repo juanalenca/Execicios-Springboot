a78979ee427de533badaf70776629c03e437634e
juanalenca <juanbarros.dev@gmail.com>
2024-10-29 21:44:50 -0300

    Retirando um import desnecessário

--- exercicio_ssb/src/main/java/com/example/exerciciossb/controllers/ProdutoController.java
+++ exercicio_ssb/src/main/java/com/example/exerciciossb/controllers/ProdutoController.java
@@ -3,7 +3,6 @@ package com.example.exerciciossb.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
