05c8d01222088fff7cb7aec0ed48c2451518fcd1
juanalenca <juanbarros.dev@gmail.com>
2024-11-01 22:43:41 -0300

    Criando a função que pagina os dados

--- exercicio_ssb/src/main/java/com/example/exerciciossb/model/repositories/ProdutoRepository.java
+++ exercicio_ssb/src/main/java/com/example/exerciciossb/model/repositories/ProdutoRepository.java
@@ -1,10 +1,11 @@
 package com.example.exerciciossb.model.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.example.exerciciossb.model.entities.Produto;
 
-public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
+public interface ProdutoRepository 
+	extends PagingAndSortingRepository<Produto, Integer> {
 	
 	
 
