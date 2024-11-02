cc7f5ee259b680c8726005847a9cda50221bbba1
juanalenca <juanbarros.dev@gmail.com>
2024-11-01 22:04:47 -0300

    Reutilizando uma função existente para realizar duas funções

--- exercicio_ssb/src/main/java/com/example/exerciciossb/controllers/ProdutoController.java
+++ exercicio_ssb/src/main/java/com/example/exerciciossb/controllers/ProdutoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,8 +25,8 @@ public class ProdutoController {
 	@Autowired
 	private ProdutoRepository produtoRepository;
 
-	@PostMapping
-	public @ResponseBody Produto novoProduto(@Valid Produto produto) {	
+	@RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
+	public @ResponseBody Produto salvarProduto(@Valid Produto produto) {	
 		
 		//perciste o produto no mysql e o id retorna de forma correta
 		produtoRepository.save(produto);
@@ -43,10 +44,10 @@ public class ProdutoController {
 		return produtoRepository.findById(id);
 	}
 	
-	@PutMapping
-	public Produto alterarProduto(@Valid Produto produto) {
-		produtoRepository.save(produto);
-		return produto;
-	}
+//	@PutMapping
+//	public Produto alterarProduto(@Valid Produto produto) {
+//		produtoRepository.save(produto);
+//		return produto;
+//	}
 	
 }
